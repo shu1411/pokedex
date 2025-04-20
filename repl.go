13299d44cf6247def3e1fc4f8d1f3f8effc28ad4
shu <106082751+shu1411@users.noreply.github.com>
2025-04-20 17:56:11 +0200

    add basic REPL and commands

--- repl.go
+++ repl.go
@@ -1,9 +1,64 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"os"
+	"strings"
+	"fmt"
+)
+
+const cli_prompt = "Pokedex > "
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print(cli_prompt)
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+		
+		cmdName := words[0]
+
+		cmd, exists := getCommands()[cmdName]
+		if exists {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+type cliCommand struct {
+	name		string
+	description string
+	callback	func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:		 "help",
+			description: "Displays a help message",
+			callback:	 commandHelp,
+		},
+		"exit": {
+			name:		 "exit",
+			description: "Exit the Pokedex",
+			callback: 	 commandExit,	
+		},
+	}
 }
